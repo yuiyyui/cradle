90c8f5b3d36754dc3faa5336c5a03295979419bd
yuiyyui <164984141+yuiyyui@users.noreply.github.com>
2024-05-08 10:08:34 +0900

    Update study.py

--- study.py
+++ study.py
@@ -2,5 +2,12 @@ import sys
 
 x=int(input("x:"))
 y=int(input("z:"))
-
+x=int(input("x:"))
+y=int(input("z:"))
+x=int(input("x:"))
+y=int(input("z:"))
+x=int(input("x:"))
+y=int(input("z:"))
+x=int(input("x:"))
+y=int(input("z:"))
 
